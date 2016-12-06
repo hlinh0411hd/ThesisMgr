666d869cef6639bd68a9b53017e823caf2840cda
thanhbinh7099 <thanhbinh7099@gmail.com>
2016-12-06 23:16:31 +0700

    Add files via upload

--- application/views/mainpage/mainpage.php
+++ application/views/mainpage/mainpage.php
@@ -1,6 +1,26 @@
 <div id="content" class="center row">
     <div class="container">
         <div id="showTinTuc" class="col-sm-8">
+            <div id="carousel-id" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-id" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-id" data-slide-to="1" class=""></li>
+                    <li data-target="#carousel-id" data-slide-to="2" class=""></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img alt="First slide" class="img-responsive" height="450px" src="public/img/uet_slide/uet_vn_1.jpg">
+                    </div>
+                    <div class="item">
+                        <img alt="Second slide" class="img-responsive" height="450px" src="public/img/uet_slide/uet_vn_2.JPG">
+                    </div>
+                    <div class="item">
+                        <img alt="Third slide" class="img-responsive" height="450px" src="public/img/uet_slide/uet_vn_3.jpg">
+                    </div>
+                </div>
+                <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div>
         </div>
         <div id="tinTuc" class="col-sm-4">
             <div class="panel panel-primary">
