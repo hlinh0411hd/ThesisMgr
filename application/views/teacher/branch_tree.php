6ead6778d2f84521b6c9442b9fc5e58bbc651f7e
thanhbinh7099 <thanhbinh7099@gmail.com>
2016-12-06 23:18:47 +0700

    Add files via upload

--- application/views/teacher/branch_tree.php
+++ application/views/teacher/branch_tree.php
@@ -9,59 +9,62 @@ $r = rand(0,100000);
 if (isset($item))
 extract($item);
 ?>
-<form id="renew<?php echo $r; ?>" onsubmit="createNew('#renew<?php echo $r; ?>');return false;">
-    <div class="form-group row">
-        <div class="col-sm-4">
-            <input id="researchDirectionName" name="researchDirectionName" class="form-control" placeholder="Tên hướng nghiên cứu"
-            <?php
-                if (isset($researchDirectionName)){
-                    echo 'value="'.$researchDirectionName.'" disabled';
-                }
-            ?>
-            >
-        </div>
-        <div class="col-sm-8">
-            <div id="field_branch<?php echo $r; ?>">
+<div style="padding: 10px;">
+    <form id="renew<?php echo $r; ?>" onsubmit="createNew('#renew<?php echo $r; ?>');return false;">
+        <div class="form-group row">
+            <div class="col-sm-4">
+                <input id="researchDirectionName" name="researchDirectionName" class="form-control" placeholder="Tên hướng nghiên cứu"
                 <?php
-                if (isset($relativeFieldList))
-                    foreach ($relativeFieldList[$researchDirectionId] as $relativeField){
-                        $data['relativeField'] = $relativeField;
-                        $data['fieldList'] = $fieldList;
-                        $this->load->view('teacher/branch_field',$data);
+                    if (isset($researchDirectionName)){
+                        echo 'value="'.$researchDirectionName.'" disabled';
                     }
                 ?>
+                >
             </div>
-            <button type="button" class="btn btn-info" onclick="addFieldBranch('#field_branch<?php echo $r; ?>');"
-                <?php
-                if (isset($researchDirectionName)){
-                    echo 'style="display:none"';
-                }
-                ?>
-            > + Thêm lĩnh vực</button>
+            <div class="col-sm-8">
+                <div id="field_branch<?php echo $r; ?>">
+                    <?php
+                    if (isset($relativeFieldList))
+                        foreach ($relativeFieldList[$researchDirectionId] as $relativeField){
+                            $data['relativeField'] = $relativeField;
+                            $data['fieldList'] = $fieldList;
+                            $this->load->view('teacher/branch_field',$data);
+                        }
+                    ?>
+                </div>
+                <button type="button" class="btn btn-info" onclick="addFieldBranch('#field_branch<?php echo $r; ?>');"
+                    <?php
+                    if (isset($researchDirectionName)){
+                        echo 'style="display:none"';
+                    }
+                    ?>
+                > + Thêm lĩnh vực</button>
+            </div>
         </div>
-    </div>
-    <div class="form-group"
-        <?php
-        if (isset($researchDirectionName)){
-            echo 'style="display:none"';
-        }?>
-        >
-        <button type="button" class="btn btn-danger" onclick="deleteBranch('#renew<?php echo $r; ?>')">Xóa</button>
-        <button type="button" class="btn btn-success" onclick="<?php
+        <div class="form-group"
+            <?php
             if (isset($researchDirectionName)){
-                echo "updateTree('#renew".$r."',".$researchDirectionId.")";
-            } else {
-                echo "saveTree('#renew".$r."')";
-            }
-        ?>">Lưu</button>
-    </div>
-    <div id="edit"<?php
-    if (!isset($researchDirectionName)){
-        echo 'style="display:none"';
-    }?>>
-        <button class='btn' onclick='enableInputTree("#renew<?php echo $r; ?>")'>Chỉnh sửa</button>
-        <button class='btn' onclick='deleteBranchTree(<?php echo $researchDirectionId;?>)'>Xóa</button>
-    </div>
-    <hr/>
+                echo 'style="display:none"';
+            }?>
+            >
+            <button type="button" class="btn btn-danger" onclick="deleteBranch('#renew<?php echo $r; ?>')">Xóa</button>
+            <button type="button" class="btn btn-success" onclick="<?php
+                if (isset($researchDirectionName)){
+                    echo "updateTree('#renew".$r."',".$researchDirectionId.")";
+                } else {
+                    echo "saveTree('#renew".$r."')";
+                }
+            ?>">Lưu</button>
+        </div>
+        <div id="edit"<?php
+        if (!isset($researchDirectionName)){
+            echo 'style="display:none"';
+        }?>>
+            <button class='btn btn-warning' onclick='enableInputTree("#renew<?php echo $r; ?>")'>Chỉnh sửa</button>
+            <button class='btn btn-danger' onclick='deleteBranchTree(<?php echo $researchDirectionId;?>)'>Xóa</button>
+        </div>
+        <hr/>
 
-</form>
+    </form>
+
+</div>
