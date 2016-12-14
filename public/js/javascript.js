87bca3ff83e175d8e1a36b521dea3edba79277ba
thanhbinh7099 <thanhbinh7099@gmail.com>
2016-12-15 00:32:30 +0700

    Add files via upload

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,3 +1,12 @@
+$(document).ready(function(e){
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+        e.preventDefault();
+        var param = $(this).attr("href").replace("#","");
+        var concept = $(this).text();
+        $('.search-panel span#search_concept').text(concept);
+        $('.input-group #search_param').val(param);
+    });
+});
 $(function () {
 	$('input').iCheck({
 		checkboxClass: 'icheckbox_square-blue',
